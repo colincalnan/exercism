b48bcafa618498c132370193660f60b75a500675
Colin Calnan <colin@domain7.com>
2021-07-09 13:51:07 -0700

    Just removing the uneeded parenthesis.

---
+++ javascript/reverse-string/reverse-string.js
@@ -0,0 +1,6 @@
+/**
+ * Function to reverse a string
+ * @param {string} str - The input string.
+ * @returns {string} The reversed string.
+ */
+export const reverseString = str => [...str].reverse().join("")
