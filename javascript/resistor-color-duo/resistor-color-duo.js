9be6986877004c6f133b220db085e5e82fba0129
Colin Calnan <colin@domain7.com>
2021-07-14 13:55:46 -0700

    updating as per comment - https://exercism.io/my/solutions/edf772f4eb3f4d1f9021a2b0956663d3

--- javascript/resistor-color-duo/resistor-color-duo.js
+++ javascript/resistor-color-duo/resistor-color-duo.js
@@ -1,4 +1,3 @@
-
 export const COLORS = [
   'black',
   'brown',
@@ -13,15 +12,33 @@ export const COLORS = [
 ];
 
 /**
+ * Function to get the index of a color
+ * @param {string} color 
+ * @returns {integer} index of passed color in list of colors
+ */
+function getColorIndex(color) {
+  return COLORS.indexOf(color);
+}
+
+/**
+ * In postional numeral systems you take it's position and use that as the exponentation of 10
+ * So in the case of 68 = 6 x 10^1 + 8 x 10^0 = 60 + 8 = 68!
+ * @param {*} integerValues 
+ * @returns 
+ */
+function convertToInteger(integerValues) {
+  let accumulator = 0;
+  integerValues.forEach((integer, index) => accumulator += integer * Math.pow(10, (integerValues.length - 1) - index));
+  return accumulator;
+}
+
+/**
  * Function to decode colors to a single integer
  * @param {array} colors 
  * @returns {int} 
  */
-export const decodedValue = (colors) => { 
-  return parseInt(colors
-    .slice(0,2)
-    .filter(color => COLORS.indexOf(color) != -1)
-    .map(color => COLORS.indexOf(color))
-    .join('')
-  );
+export const decodedValue = (colors) => {
+  let integerValues = [];
+  colors.splice(0, 2).forEach( color => integerValues.push(getColorIndex(color)));
+  return convertToInteger(integerValues);
 };
