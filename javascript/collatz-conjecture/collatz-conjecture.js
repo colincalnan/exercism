d463c29dae37ff208d7bbea35c50d80d3afb495e
Colin Calnan <colin@domain7.com>
2021-08-25 09:50:38 -0700

    Updating to be more verbose, as opposed to being clever, helps with readability and understanding.

--- javascript/collatz-conjecture/collatz-conjecture.js
+++ javascript/collatz-conjecture/collatz-conjecture.js
@@ -1,12 +1,23 @@
-const getNumberOfSteps = n => {
-  let count = 0;
-  if (n !== 1) {
-    count = 1 + getNumberOfSteps(!(n % 2) ? n / 2 : 3 * n + 1)
-  }
- return count;
-}
-
 export const steps = n => {
+  
+  const getNumberOfSteps = number => {
+    let count = 0;
+    // If we have reached 1, return
+    if (number === 1) { 
+      return count;
+    // otherwise keep going until we reach 1
+    } else {
+      // if n is divisble by 2 (even) then divide it by two
+      if (number % 2 <= 0) { 
+        number = number / 2; 
+      // otherwise return 3n + 1
+      } else {
+        number = (3 * number) + 1;
+      }
+      return 1 + getNumberOfSteps(number);
+    }
+  }
+
   if (n <= 0) throw new Error('Only positive numbers are allowed');
   return getNumberOfSteps(n) 
 };
