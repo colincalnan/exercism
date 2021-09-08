48222a4373177bc66405719d7abff8e209415283
Colin Calnan <colin.calnan@gmail.com>
2021-09-08 10:15:40 -0700

    Updating space-age.js

--- javascript/space-age/space-age.js
+++ javascript/space-age/space-age.js
@@ -1,16 +1,19 @@
-// Learned the Unary plus operator from @slaymance, thank you!!
-// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Unary_plus
-
 const EARTH_YEAR_IN_SECONDS = 31557600
-const ORBITAL_YEARS = new Map([
-  ['mercury', 0.2408467],
-  ['venus', 0.61519726],
-  ['earth', 1],
-  ['mars', 1.8808158],
-  ['jupiter', 11.862615],
-  ['saturn', 29.447498],
-  ['uranus', 84.016846],
-  ['neptune', 164.79132]
-])
+const ORBITAL_YEARS = {
+  'mercury': 0.2408467,
+  'venus': 0.61519726,
+  'earth': 1,
+  'mars': 1.8808158,
+  'jupiter': 11.862615,
+  'saturn': 29.447498,
+  'uranus': 84.016846,
+  'neptune': 164.79132
+}
 
-export const age = (planet, seconds) => +(seconds/(ORBITAL_YEARS.get(planet) * EARTH_YEAR_IN_SECONDS)).toFixed(2);
+const round = (string, precision = 2) => Number(string.toFixed(precision));
+
+export const age = (planet, seconds) => {
+  const planet_orbit_in_earth_seconds = ORBITAL_YEARS[planet] * EARTH_YEAR_IN_SECONDS;
+  const seconds_in_earth_years = seconds/planet_orbit_in_earth_seconds;
+  return round(seconds_in_earth_years);
+}
